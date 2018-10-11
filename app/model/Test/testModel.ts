47297237b1484d2e6a6880ec54dbeff555e2c6e8
TeiturMcSwagger <teiturargeseloy@hotmail.com>
2018-10-11 09:41:44 +0200

    react
    I have inported the react components

--- app/model/Test/testModel.ts
+++
@@ -1,4 +0,0 @@
-export function helloRetrieve(message : String){
-    // This data is retrived from db!
-    return "Retrieved: " + message + "... from the model!";
-}
--- dist/model/Test/testModel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function helloRetrieve(message) {
-    // This data is retrived from db!
-    return "Retrieved: " + message + "... from the model!";
-}
-exports.helloRetrieve = helloRetrieve;
