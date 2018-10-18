7706234b304d37ee0098c3ee3a3702cacf06d7d0
Christian Larsen <christianlarsen@h543.aau1x.klient.slv312.site.aau.dk>
2018-10-18 11:56:50 +0200

    service

---
+++ src/services/groupService.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import axios from 'axios'
+
+class groupService {
+    constructor(props) {
+        //setup somekind of connection i guess?
+    }
+
+    //TODO: Implement these CRUD methods
+    addGroupDB() {
+        axios.post()
+    }
+    updateGroupDB() {
+        axios.post()
+    }
+    deleteGroupDB() {
+        axios.delete()
+    }
+    getGroupDB() {
+        axios.get()
+    }
+}
