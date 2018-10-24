574ed2aae8b8b3c36492c5c2bf5ccae9ec94cf78
KornephorosDk <dala15@student.aau.dk>
2018-10-24 09:16:29 +0200

    Cleaned earlier push, fixed merge conflicts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
-<<<<<<< HEAD
-import logo from './logo.svg';
-
-
-=======
 import GroupPageContainer from './Group/GroupPageContainer';
 import logo from './logo.svg';
->>>>>>> bc77f5ff29ff42c2a2996a13bc1771ba0d16f861
 
 class App extends React.Component {
   public render() {
@@ -21,8 +15,6 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-<<<<<<< HEAD
-=======
         <Router>
           {/* <Switch>           */}
             {/* The commented routes below should be un-commented and modified to point to the correct component
@@ -32,7 +24,6 @@ class App extends React.Component {
             <Route path="/groups/:group_id" component={GroupPageContainer} />
           {/* </Switch> */}
         </Router>
->>>>>>> bc77f5ff29ff42c2a2996a13bc1771ba0d16f861
       </div>
     );
   }
