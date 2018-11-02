6a13f128181b4d3583924ef0e8eac2a219d93eaa
Lau Kondrup <lau.kondrup@gmail.com>
2018-11-02 11:18:45 +0100

    POST groups/create

--- src/services/groupServiceApi.tsx
+++ src/services/groupServiceApi.tsx
@@ -24,6 +24,6 @@ export class GroupServiceApi implements GroupService{
         });
     }
     public async createGroup(group : any) {
-        return axios.post("/groups/", group);
+        return axios.post("/groups/create", group);
     }
 }
