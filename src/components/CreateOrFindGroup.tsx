093324cb3dca94b201c5a32af772f60b9c8ffa44
peter <pmadse13@student.aau.dk>
2018-11-07 10:08:34 +0100

    should use history for routing otherwise wont work  on firefox????

--- src/components/CreateOrFindGroup.tsx
+++ src/components/CreateOrFindGroup.tsx
@@ -1,18 +1,24 @@
 import * as React from "react";
 import { Button } from "antd";
-import { Link } from "react-router-dom";
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
 
-export default class CreateOrFindGroup extends React.Component {
+export default withRouter (class CreateOrFindGroup extends React.Component<RouteComponentProps> {
   public render() {
     return (
       <div>
-        <Button size="large">
-          <Link to="/groups">Find group</Link>
+        <Button size="large" onClick={this.handleFindGroupClicked}>
+          Find group
         </Button>
-        <Button size="large">
-          <Link to="/create">Create group</Link>
+        <Button size="large" onClick={this.handleCreateGroupClicked}>
+          Create group
         </Button>
       </div>
     );
   }
-}
+  private handleCreateGroupClicked = () => {
+    this.props.history.push("/create");
+  };
+  private handleFindGroupClicked = () => {
+    this.props.history.push("/groups");
+  };
+})
